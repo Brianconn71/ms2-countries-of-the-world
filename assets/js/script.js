aaeb309ee0a90b1d02cf9bf473900e76efab974c
Brianconn71 <Brian.connolly71@gmail.com>
2021-01-19 15:55:22 +0000

    edit: added more advanced styling to the contact form, added the sendMail function to the script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,8 @@ $(document).ready(function(){
     });
 });   
 
+
+// This jQuery function adds a thank you message in a hidden div after a user submits their form and hides the form
 $(document).ready(function(){
     $("form").submit(function(){
         $(".contact-form").hide();
@@ -41,4 +43,21 @@ function changeImage(){
 function resetInterval(){
     clearInterval(interval)
     interval = setInterval(run, 5000)
+}
+//This function will send a filled out contact form to mail.js
+
+function sendMail(contactForm){
+    emailjs.send("service_9fhvo61", "template_pcsmdnv",{
+        "from_name": contactForm.first-name.value,
+        "from_email": contactForm.email.value,
+        "Message": contactForm.reason.value,
+    }).then(
+        function(response){
+            console.log("success", response);
+        },
+        function(error){
+            console.log("Fail", error)
+        }
+    )
+    return false;
 }
