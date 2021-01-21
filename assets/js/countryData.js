2b91842ba420c4e8489a6d9ef944d027257bb10b
Brianconn71 <Brian.connolly71@gmail.com>
2021-01-21 18:04:10 +0000

    edit: Got the flags of the countries and some basic info to display in the cards from the search bar, styling needed

--- assets/js/countryData.js
+++ assets/js/countryData.js
@@ -1,33 +1,4 @@
-// function countryInfoHTML(country){
-//     console.log(country.name)
-// }
 
-// function getCountryData(event){
-//     var countrySearched = $('search').val()
-//     $.when(
-//         $.getJSON('https://restcountries.eu/rest/v2/all')
-//     ).then(
-//         function(response){
-//             var data = response;
-//             $("#country-search").html(countryInfoHTML(data));
-//         }, function(errorResponse){
-//             if (errorResponse.status ===404){
-//                 $("#country-search").html(`<h2>No Country called ${countrySearched}</h2>`)
-//             } else{
-//                 console.log(errorResponse);
-//                 $("#country-search").html(`error! ${errorResponse.responseJSON.message}`)
-//             }
-//         }
-//     )
-// }
-// const API_URL = 'https://restcountries.eu/rest/v2/all'
-// const search_by_name = 'https://restcountries.eu/rest/v2/name/{name}'
-
-// const search_box = document.getElementById('search-box')
-
-// async function getCountryData(){
-
-// }
 //Spent way too long stuck here on an issue with jQuery, a very simple fix to was found here https://stackoverflow.com/questions/18271251/typeerror-ajax-is-not-a-function
 
 
@@ -40,7 +11,14 @@ $(document).ready(function(){
             url: `https://restcountries.eu/rest/v2/name/${searchValue}?fullText=true `,
         }).done(function(name){
             $('#country-search').html(`
-                ${name[0].capital}
+            <div class="card" style="width: 18rem;">
+                <img src="${name[0].flag}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${name[0].name}</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
             `)
         });
     })
