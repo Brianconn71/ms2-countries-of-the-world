a4ec0f30474dce7aae8d5830cb6cdd9e786555e2
Brianconn71 <Brian.connolly71@gmail.com>
2021-01-22 21:30:45 +0000

    styling: finished styling the cards that the search bar returns when a country is searched for

--- assets/js/countryData.js
+++ assets/js/countryData.js
@@ -1,4 +1,3 @@
-
 //Spent way too long stuck here on an issue with jQuery, a very simple fix to was found here https://stackoverflow.com/questions/18271251/typeerror-ajax-is-not-a-function
 
 
@@ -8,15 +7,20 @@ $(document).ready(function(){
 
         // Make ajax request
         $.ajax({
-            url: `https://restcountries.eu/rest/v2/name/${searchValue}?fullText=true `,
-        }).done(function(name){
+            url: `https://restcountries.eu/rest/v2/name/${searchValue}`,
+        }).done(function(data){
             $('#country-search').html(`
-            <div class="card" style="width: 18rem;">
-                <img src="${name[0].flag}" class="card-img-top" alt="...">
+            <div class="card country-card">
+                <img src="${data[0].flag}" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">${name[0].name}</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <h5 class="card-title country-title">${data[0].name}</h5>
+                    <p class="card-text">
+                        <ul class="basic-info">
+                            <li class="general-info">Capital City: ${data[0].capital}</li>
+                            <li class="general-info">Continent: ${data[0].region}</li>
+                            <li class="general-info">Population: ${data[0].population}</li>
+                        </ul>
+                    </p>
                 </div>
             </div>
             `)
