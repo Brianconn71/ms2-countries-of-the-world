c107df5837433063cceb086e1cdfccfd34411475
Brianconn71 <Brian.connolly71@gmail.com>
2021-01-23 18:53:04 +0000

    feat: Have the africa button working correctly, gets back the correct information, need to do some styling with it

--- assets/js/countryData.js
+++ assets/js/countryData.js
@@ -26,4 +26,39 @@ $(document).ready(function(){
             `)
         });
     })
+
 })
+
+var loadAfrica = function(){
+$(document).ready(function(){
+    $('#africa-btn').on('click', function(event){
+        // Make ajax request
+        $.ajax({
+            url: `https://restcountries.eu/rest/v2/region/africa`,
+        }).done(function(data){
+            data.forEach(function(m){
+                document.getElementById('africa').innerHTML += `
+                <div class="card country-card">
+                <img src="${m[Object.keys(m)[21]]}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title country-title">${m[Object.keys(m)[0]]}</h5>
+                    <p class="card-text">
+                        <ul class="basic-info">
+                            <li class="general-info">Capital City: ${m[Object.keys(m)[5]]}</li>
+                            <li class="general-info">Sub-Continent: ${m[Object.keys(m)[8]]}</li>
+                            <li class="general-info">Population: ${m[Object.keys(m)[9]]}</li>
+                        </ul>
+                    </p>
+                </div>
+            </div>               
+                `
+            
+            // $('#africa').text(
+            // `${m[Object.keys(m)[0]]}`
+            // )
+            })
+        });
+    })
+
+})
+}
