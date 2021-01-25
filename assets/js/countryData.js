f8f98bb3e064e093f6c29c01fea5d00b74098702
Brianconn71 <Brian.connolly71@gmail.com>
2021-01-25 18:11:15 +0000

    edit: decided to make a synchronous call to the api by click of button so edited out the asynchronus call in the function, added a close/clear button to allow users to close the display of countries after clickuing continent button

--- assets/js/countryData.js
+++ assets/js/countryData.js
@@ -1,6 +1,4 @@
 //Spent way too long stuck here on an issue with jQuery, a very simple fix to was found here https://stackoverflow.com/questions/18271251/typeerror-ajax-is-not-a-function
-
-
 $(document).ready(function(){
     $('#search').on('keyup', function(event){
         let searchValue = event.target.value;
@@ -30,18 +28,45 @@ $(document).ready(function(){
 })
 
 
-    $(document).ready(function(){
+
         $('#africa-btn').on("click",function(event){
             // Make ajax request
-            $.ajax({
-                url: `https://restcountries.eu/rest/v2/region/africa`,
-            }).done(function(data){
-                data.forEach(function(m){
+            // $.ajax({
+            //     url: `https://restcountries.eu/rest/v2/region/africa`,
+            // }).done(function(data){
+            //     data.forEach(function(m){
+            //         document.getElementById('africa').innerHTML += `
+            //         <div class="container" id="row-country">
+            //             <button class="continent-btn" id="close" onclick="sayHello()">Close</button>
+            //             <div class="row">
+            //                 <div class="col-3 d-flex align-items-center">
+            //                     <img src="${m[Object.keys(m)[21]]}" class="img-thumbnail" alt="flags of africa">
+            //                 </div>
+            //                 <div class="col-9">
+            //                     <h5 class="country-title">${m[Object.keys(m)[0]]}</h5>
+            //                     <p class="card-text">
+            //                         <ul class="basic-info">
+            //                             <li class="general-info">Capital City: ${m[Object.keys(m)[5]]}</li>
+            //                             <li class="general-info">Sub-Continent: ${m[Object.keys(m)[8]]}</li>
+            //                             <li class="general-info">Population: ${m[Object.keys(m)[9]]}</li>
+            //                         </ul>
+            //                     </p>
+            //                 </div>
+            //             </div>
+                        
+            //         </div>              
+            //         `
+            //     })
+            
+            fetch('https://restcountries.eu/rest/v2/region/africa')
+  .then((r) => r.json())
+  .then((data) =>  data.forEach(function(m){
                     document.getElementById('africa').innerHTML += `
                     <div class="container" id="row-country">
+                    <button class="continent-btn" id="close" onclick="sayHello()">Close</button>
                         <div class="row">
                             <div class="col-3 d-flex align-items-center">
-                                <img src="${m[Object.keys(m)[21]]}" class="img-thumbnail" alt="flags of africa">
+                                <img src="${m[Object.keys(m)[21]]}" class="img-fluid" alt="flags of africa">
                             </div>
                             <div class="col-9">
                                 <h5 class="country-title">${m[Object.keys(m)[0]]}</h5>
@@ -57,10 +82,9 @@ $(document).ready(function(){
                         
                     </div>              
                     `
-                })
-            });
+                }))
+  .catch((e) => console.log('Booo'));
         })
-})
 
 $(document).ready(function(){
         $('#asia-btn').on("click",function(event){
@@ -160,10 +184,35 @@ $(document).ready(function(){
 $(document).ready(function(){
         $('#oceania-btn').on("click",function(event){
             // Make ajax request
-            $.ajax({
-                url: `https://restcountries.eu/rest/v2/region/oceania`,
-            }).done(function(data){
-                data.forEach(function(m){
+            // $.ajax({
+            //     url: `https://restcountries.eu/rest/v2/region/oceania`,
+            // }).done(function(data){
+            //     data.forEach(function(m){
+            //         document.getElementById('oceania').innerHTML += `
+            //         <div class="container" id="row-country">
+            //             <div class="row">
+            //                 <div class="col-3 d-flex align-items-center">
+            //                     <img src="${m[Object.keys(m)[21]]}" class="img-fluid" alt="flags of africa">
+            //                 </div>
+            //                 <div class="col-9">
+            //                     <h5 class="country-title">${m[Object.keys(m)[0]]}</h5>
+            //                     <p class="card-text">
+            //                         <ul class="basic-info">
+            //                             <li class="general-info">Capital City: ${m[Object.keys(m)[5]]}</li>
+            //                             <li class="general-info">Sub-Continent: ${m[Object.keys(m)[8]]}</li>
+            //                             <li class="general-info">Population: ${m[Object.keys(m)[9]]}</li>
+            //                         </ul>
+            //                     </p>
+            //                 </div>
+            //             </div>
+                        
+            //         </div>              
+            //         `
+            //     })
+            // });
+            fetch('https://restcountries.eu/rest/v2/region/oceania')
+  .then((r) => r.json())
+  .then((data) =>  data.forEach(function(m){
                     document.getElementById('oceania').innerHTML += `
                     <div class="container" id="row-country">
                         <div class="row">
@@ -184,7 +233,13 @@ $(document).ready(function(){
                         
                     </div>              
                     `
-                })
-            });
+                }))
+  .catch((e) => console.log('Booo'));
         })
 })
+
+function sayHello(){
+     document.getElementById('africa').innerHTML = "";
+     
+    
+}
