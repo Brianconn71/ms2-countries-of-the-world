27727e527b83c1567ff78f90baf8c650cbacbc7d
Brianconn71 <Brian.connolly71@gmail.com>
2021-01-28 00:22:42 +0000

    feat: got a basic function working to show the quiz question, calll an api and display the question, answers yet to be added

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -2,6 +2,8 @@
 const startBtn = document.getElementById('start-btn');
 const questionContainer = document.getElementById('question-container')
 
+let questions = document.getElementById('questions')
+// questions.addEventListener('click', nextQuestion),
 startBtn.addEventListener('click', startQuiz)
 
 function startQuiz(){
@@ -11,8 +13,33 @@ function startQuiz(){
     nextQuestion()
 }
 
-function nextQuestion(){
+ function nextQuestion(){
+    fetch('https://opentdb.com/api.php?amount=10&category=22&difficulty=easy&type=multiple')
+    .then(res => {
+        return res.json();
+    })
+    .then(loadedQuestions => {
+        console.log(loadedQuestions.results);
+        questions = loadedQuestions.results.map(loadedQuestions => {
+            const formattedQuestion = {
+                question: loadedQuestions.question
+            };
+            questions.innerHTML = formattedQuestion.question
+             const answerChoices = [loadedQuestions.incorrect_answers];
+             formattedQuestion.answer = Math.floor(Math.random()*3) +1;
+             answerChoices.splice(formattedQuestion.answer - 1, 0, loadedQuestions.correct_answer);
 
+            answerChoices.forEach((choice, index) => {
+                 formattedQuestion['choice' + (index +1)] - choice
+             })
+
+             return formattedQuestion;
+         });
+         startQuiz();
+    })
+    .catch(err => {
+        console.log(err)
+    })
 }
 
 function answerSelected(){
