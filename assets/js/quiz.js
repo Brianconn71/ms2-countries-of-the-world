75a37ea6be003c417055d6f08b06da7fffa3b8a8
Brianconn71 <Brian.connolly71@gmail.com>
2021-01-28 23:34:07 +0000

    fix: fixed an issue with quiz questions not displaying properly, still an issue remains with the answers

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -3,37 +3,54 @@ const startBtn = document.getElementById('start-btn');
 const questionContainer = document.getElementById('question-container')
 
 let questions = document.getElementById('questions')
+const answerBtns = Array.from(document.getElementsByClassName('answer-btn'));
+
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+
+
+
 // questions.addEventListener('click', nextQuestion),
 startBtn.addEventListener('click', startQuiz)
 
 function startQuiz(){
-    console.log('started')
+    questionCounter = 0;
+    score = 0;
     startBtn.classList.add('hide')
     questionContainer.classList.remove('hide')
     nextQuestion()
 }
 
  function nextQuestion(){
+     questionCounter++
     fetch('https://opentdb.com/api.php?amount=10&category=22&difficulty=easy&type=multiple')
     .then(res => {
         return res.json();
     })
     .then(loadedQuestions => {
-        console.log(loadedQuestions.results);
         questions = loadedQuestions.results.map(loadedQuestions => {
             const formattedQuestion = {
                 question: loadedQuestions.question
             };
             questions.innerHTML = formattedQuestion.question
-             const answerChoices = [loadedQuestions.incorrect_answers];
+             const answerChoices = [...loadedQuestions.incorrect_answers];
              formattedQuestion.answer = Math.floor(Math.random()*3) +1;
              answerChoices.splice(formattedQuestion.answer - 1, 0, loadedQuestions.correct_answer);
-
-            answerChoices.forEach((choice, index) => {
-                 formattedQuestion['choice' + (index +1)] - choice
+             
+            //  answerBtns.forEach( choice => {
+            //      const number = choice.dataset['number'];
+            //      choice.innerText = answerChoices[number]
+            //     })
+            answerbtns.forEach((choice, index) => {
+                formattedQuestion['choice' + (index +1)] - choice
              })
-
              return formattedQuestion;
+             choice.innerText = formattedQuestion
          });
          startQuiz();
     })
