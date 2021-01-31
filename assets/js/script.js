b25c819163094246fd44471af4b1db0c773199a3
Brianconn71 <Brian.connolly71@gmail.com>
2021-01-31 20:02:59 +0000

    feat: added a return to top button at the bottom of the page that displays when a user scrolls down the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
+// function for scrolling back to the top of the page if the user clicks on the return to top button
+scrollButton = document.getElementById('pageTop');
+
+window.onscroll = function() {scrollFunction()};
+
+// user scrolls 50px down page then button will appear
+function scrollFunction(){
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
+        scrollButton.style.display = 'block';
+    } else {
+        scrollButton.style.display = 'none'
+    }
+}
+
+function topOfPage(){
+    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+}
+
 // A jQuery function to move the page down to the search section once the user clicks on the header cta button
 // I spent some time figuring this out but eventually I got my answer here: https://stackoverflow.com/questions/2238368/how-to-make-a-button-redirect-to-another-page-using-jquery-or-just-javascript
 $(document).ready(function(){
