37f1a3f3b215999668861dbfa2407b6d110779da
Brianconn71 <Brian.connolly71@gmail.com>
2021-02-03 12:44:03 +0000

    feat: finally managed to get my quiz game to work properly, fetching questions from the geogrphy quiz api on open trivia database, styling is needed as its basic so far

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -3,13 +3,17 @@
 // Constants
 const question = document.getElementById('question');
 const startBtn = document.getElementById('start-btn');
+const answerBtns = document.getElementById('answer-btns')
 const questionContainer = document.getElementById('question-container');
 const answers = Array.from(document.getElementsByClassName('answer-btn'));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score')
 const fullBar = document.getElementById('full-bar')
-const max_questions = 3;
-const correct_bonus = 10
+const finishedGame = document.getElementById('finishedGame')
+const max_questions = 10;
+const correct_bonus = 10;
+const finalScore = document.getElementById('finalScore')
+const newScore = localStorage.getItem('newScore')
 
 // lets
 let currentQuestion = {};
@@ -18,36 +22,41 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-    {
-        question: 'Inside which HTML element do we put the JavaScript??',
-        choice1: '<script>',
-        choice2: '<javascript>',
-        choice3: '<js>',
-        choice4: '<scripting>',
-        answer: 1,
-    },
-    {
-        question:
-            "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        choice1: "<script href='xxx.js'>",
-        choice2: "<script name='xxx.js'>",
-        choice3: "<script src='xxx.js'>",
-        choice4: "<script file='xxx.js'>",
-        answer: 3,
-    },
-    {
-        question: " How do you write 'Hello World' in an alert box?",
-        choice1: "msgBox('Hello World');",
-        choice2: "alertBox('Hello World');",
-        choice3: "msg('Hello World');",
-        choice4: "alert('Hello World');",
-        answer: 4,
-    },
-];
+let questions = [];
 
-// // questions.addEventListener('click', nextQuestion),
+// questions.addEventListener('click', nextQuestion),
 startBtn.addEventListener('click', startQuiz)
+
+
+// the below fetch, gets the questions and answers for the quiz form the open trivia database https://opentdb.com/
+//Struggled badly with the quiz initially before I got inspiration and guidance from the set of videos on youtube located here: https://www.youtube.com/watch?v=3aKOQn2NPFs&list=PLB6wlEeCDJ5Yyh6P2N6Q_9JijB6v4UejF&index=11&ab_channel=JamesQQuick
+
+fetch('https://opentdb.com/api.php?amount=10&category=22&difficulty=easy&type=multiple')
+     .then((res) => {
+         return res.json();
+     })
+     .then((loadedQuestions) => {
+         questions = loadedQuestions.results.map(loadedQuestion => {
+             const formattedQuestion = {
+                 question: loadedQuestion.question
+             };
+             //questions.innerHTML = formattedQuestion.question
+              const answerChoices = [...loadedQuestion.incorrect_answers];
+              formattedQuestion.answer = Math.floor(Math.random()*4) +1;
+              answerChoices.splice(formattedQuestion.answer - 1, 0, loadedQuestion.correct_answer);
+            
+             answerChoices.forEach((choice, index) => {
+                 formattedQuestion['choice' + (index +1)] = choice
+              });
+             
+             //  console.log(formattedQuestion)
+              return formattedQuestion;
+          });
+     })
+     .catch((err) => {
+         console.error(err)
+});
+
 
 function startQuiz(){
      questionCounter = 0;
@@ -61,8 +70,9 @@ function startQuiz(){
 function nextQuestion(){
 
     if(availableQuestions.length === 0 || questionCounter >= max_questions){
-        //ges to end of page
-        return window.location.assign('/end.html')
+        // Shows a new container after a user finishes the quiz
+        questionContainer.classList.add('hide')
+        finishedGame.classList.remove('hide')
     }
       questionCounter++
       progressText.innerText = `Question ${questionCounter}/${max_questions}`;
@@ -109,39 +119,16 @@ answers.forEach(choice =>{
 })
 function incrementScore(num){
     score += num;
-    scoreText.innerText = score
+    scoreText.innerText = score;
+    // adding the last score saved in the local storage to the final score displayed on the end screen of the quiz
+    finalScore.innerText = score;
 }
-//     fetch('https://opentdb.com/api.php?amount=10&category=22&difficulty=easy&type=multiple')
-//     .then(res => {
-//         return res.json();
-//     })
-//     .then(loadedQuestions => {
-//         questions = loadedQuestions.results.map( loadedQuestion => {
-//             const formattedQuestion = {
-//                 question: loadedQuestion.question
-//             };
-//             questions.innerHTML = formattedQuestion.question
-//              const answerChoices = [...loadedQuestion.incorrect_answers];
-//              formattedQuestion.answer = Math.floor(Math.random()*3) +1;
-//              answerChoices.splice(formattedQuestion.answer - 1, 0, loadedQuestion.correct_answer);
-            
-//             answerBtns.forEach(answerBtn => {
-//                 const number = answerBtn.dataset['number'];
-//                 console.log(formattedQuestion)
-//                 //answerBtn.innerText = answerChoices
-//                 //formattedQuestion['answerBtn' + (index +1)] = answerBtn
-//              })
-             
-//             //  console.log(formattedQuestion)
-//             //  return formattedQuestion;
-//          });
-//          startQuiz();
-//     })
-//     .catch(err => {
-//         console.log(err)
-//     })
-// }
 
-// function answerSelected(){
 
-// }
+
+// my own custom function to start the game again once a user completes the quiz 
+function playAgain(){
+    questionContainer.classList.remove('hide')
+    finishedGame.classList.add('hide')
+    startQuiz()
+}
