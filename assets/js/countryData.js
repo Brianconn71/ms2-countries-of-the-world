9e0c0d513da0ceac9ef6f680564a148e02f49238
Brianconn71 <Brian.connolly71@gmail.com>
2021-02-21 18:37:37 +0000

    feat: added an if statement to my ajax call on the searchbar which won't display anything if the search bar is empty

--- assets/js/countryData.js
+++ assets/js/countryData.js
@@ -8,33 +8,38 @@ $(document).ready(function() {
     $('#search').on('keyup', function(event) {
         let searchValue = event.target.value;
 
-        // Make ajax request
-        $.ajax({
-            url: `https://restcountries.eu/rest/v2/name/${searchValue}`,
-        }).done(function(data) {
-        $('#country-search').html(`
-            <div class="card country-card">
-                <img src="${data[0].flag}" class="card-img-top" alt="${data[0].name} flag">
-                <div class="card-body">
-                    <button id="close" onclick="closeBtnSearch()"><i class="fas fa-window-close"></i></button>
-                    <h5 class="card-title country-title">${data[0].name}</h5>
-                    <p class="card-text">
-                        <ul class="basic-info">
-                            <li class="general-info">Capital City: ${data[0].capital}</li>
-                            <li class="general-info">Continent: ${data[0].region}</li>
-                            <li class="general-info">Population: ${data[0].population}</li>
-                        </ul>
-                    </p>
-                </div>
-            </div>
-            `);
-        }).fail(function(data){
+        if(!searchValue){
+            $('#country-search').html("");
+        }else{
+            // Make ajax request
+            $.ajax({
+                url: `https://restcountries.eu/rest/v2/name/${searchValue}`,
+            }).done(function(data) {
             $('#country-search').html(`
-                <div>The country you are looking for does not exist, please search again and watch your spelling!</div>
-            `);
-        });
+                <div class="card country-card">
+                    <img src="${data[0].flag}" class="card-img-top" alt="${data[0].name} flag">
+                    <div class="card-body">
+                        <button id="close" onclick="closeBtnSearch()"><i class="fas fa-window-close"></i></button>
+                        <h5 class="card-title country-title">${data[0].name}</h5>
+                        <p class="card-text">
+                            <ul class="basic-info">
+                                <li class="general-info">Capital City: ${data[0].capital}</li>
+                                <li class="general-info">Continent: ${data[0].region}</li>
+                                <li class="general-info">Population: ${data[0].population}</li>
+                            </ul>
+                        </p>
+                    </div>
+                </div>
+                `);
+            }).fail(function(data){
+                $('#country-search').html(`
+                    <div>The country you are looking for does not exist, please search again and watch your spelling!</div>
+                `);
+            });
+        };
     });
 });
+
 
 
 // The below functions are used to get information on the countries depending on which continent button that the user clicks
